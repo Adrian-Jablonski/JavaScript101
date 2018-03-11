b1ba0f11e149bcc3807b50654a5b60ee5b1f3d97
Adrian Jablonski <jablonski.adrian1@gmail.com>
2018-03-11 15:56:28 -0500

    With metric to imperial conversion.

--- Weather_Map_API/scripts/main.js
+++ Weather_Map_API/scripts/main.js
@@ -1,6 +1,15 @@
 window.onload = getLocation
 var getCurrentLocation = true;
+var degC; var degF;
+var feelsLikeC; var feelsLikeF;
+var minTempC; var minTempF;
+var maxTempC; var maxTempF;
+var currentUnit = "F";
+var localTime;
+var sunrise; var sunset;
+
 $("#check-weather").hide();
+$("#weather-information").hide();
 
 function getLocation() {
     // Finds user location
@@ -50,14 +59,19 @@ function update_location() {
             $("#weather-information").show();
             $("#status").hide();
             var condition = response.current.condition.text;
-            var degC = response.current.temp_c;
-            var feelsLikeC = response.current.feelslike_c;
-            var minTempC = response.forecast.forecastday[0].day.mintemp_c;
-            var maxTempC = response.forecast.forecastday[0].day.maxtemp_c;
-            var degF = response.current.temp_f;
+            degC = response.current.temp_c;
+            degF = response.current.temp_f;
+            feelsLikeC = response.current.feelslike_c;
+            feelsLikeF = response.current.feelslike_f;
+            minTempC = Math.round(response.forecast.forecastday[0].day.mintemp_c);
+            minTempF = Math.round(response.forecast.forecastday[0].day.mintemp_f);
+            maxTempC = Math.round(response.forecast.forecastday[0].day.maxtemp_c);
+            maxTempF = Math.round(response.forecast.forecastday[0].day.maxtemp_f);
+            sunrise = response.forecast.forecastday[0].astro.sunrise;
+            sunset = response.forecast.forecastday[0].astro.sunset;
             var currentLocation = response.location.name + ", " + response.location.region;
             var currentCountry = response.location.country;
-            var localTime = response.location.localtime;
+            localTime = response.location.localtime;
             // checks whether to use day or night icons
             var isDay = response.current.is_day;
             if (isDay === 1) {
@@ -68,15 +82,15 @@ function update_location() {
             }
             
             $("#condition").html(condition);
-            
-            $("#weather-box").html("<p>" + degC + "&#176; C. Feels like " + feelsLikeC + "&#176; C </p><p>max " + maxTempC + "&#176; C min " + minTempC + "&#176; C");
-            
             $("#current-location").html(currentLocation);
             $("#current-country").html(currentCountry);
             $("#local-time").html("Local Time: " + localTime);
             
             $(".background-picture").attr("src", "images/weather_pictures/" + condition +".png");
             $("#weather-icon").attr("src", "images/weather_icons/" + isDay +"/" + condition + ".png");
+            //$("#sunrise").html("Sunrise: " + sunrise);
+            
+            showWeather();
         }
 
         // handle error
@@ -90,3 +104,63 @@ function update_location() {
 
 }
 
+function imperialUnits() {
+    currentUnit = "F";
+    showWeather();
+}
+
+function metricUnits() {
+    currentUnit = "C";
+    showWeather();
+}
+
+function showWeather() {
+    if (currentUnit === "F") {
+        $("#current-temp").html(degF + "&#176;" + currentUnit);
+        $("#max-min-temp").html(maxTempF + " / " + minTempF + "&#176" + currentUnit);
+        $("#weather-box").html("<p>Feels like " + feelsLikeF + "&#176;" + currentUnit);
+        var year = localTime.slice(0, 4);
+        var monthAndDate = localTime.slice(5, 10);
+        var hour = localTime.slice(11, 13);
+        var ampm = "AM";
+        if (hour.slice(1) === ":") {
+            hour = hour[0]
+        }
+        else if (hour > 12) {
+            hour = hour - 12;
+            ampm = "PM";
+        }
+        var minutes = localTime.slice(-2)
+        $("#local-time").html("Local Time: " + monthAndDate + "-" + year + " " + hour +":" + minutes + ampm);
+        $("#sunrise").html("<b>Sunrise: </b>" + sunrise);
+        $("#sunset").html("<b>Sunset: </b>" + sunset);
+        
+        
+    }
+    else {  // if "C"
+        $("#current-temp").html(degC + "&#176;" + currentUnit);
+        $("#max-min-temp").html(maxTempC + " / " + minTempC + "&#176;" + currentUnit);
+        $("#weather-box").html("<p>Feels like " + feelsLikeC + "&#176;" + currentUnit);
+        $("#local-time").html("Local Time: " + localTime);
+        
+        var sunriseHour = sunrise.slice(0, 2);
+        var sunsetHour = sunset.slice(0, 2);
+        
+        if (sunrise.slice(-2) === "AM") {
+            var sunrise24Hr = sunrise.slice(0, 5);
+        }
+        else {
+            var sunrise24Hr = (Number(sunriseHour) + 12) + ":" + sunrise.slice(3, 5);
+        }
+        
+        if (sunset.slice(-2) === "AM") {
+            var sunset24Hr = sunset.slice(0, 5);
+        }
+        else {
+            var sunset24Hr = (Number(sunsetHour) + 12) + ":" + sunset.slice(3, 5);
+        }
+        
+        $("#sunrise").html("<b>Sunrise: </b>" + sunrise24Hr);
+        $("#sunset").html("<b>Sunset: </b>" + sunset24Hr);
+    }
+}
